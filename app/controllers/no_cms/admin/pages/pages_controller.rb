26a5c8cdd549e9d7b4ad3a66bb1b40e55c058603
David J. Brenes <david.brenes@simplelogica.net>
2014-03-05 18:38:16 +0100

    Pages tree listed

--- app/controllers/no_cms/admin/pages/pages_controller.rb
+++ app/controllers/no_cms/admin/pages/pages_controller.rb
@@ -3,6 +3,7 @@ require_dependency "no_cms/admin/pages/application_controller"
 module NoCms::Admin::Pages
   class PagesController < ApplicationController
     def index
+      @roots = NoCms::Pages::Page.roots
     end
 
     def new
