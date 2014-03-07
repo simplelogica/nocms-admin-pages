d5de5e2ad8086d6c6ff2854461db30d753529053
David J. Brenes <david.brenes@simplelogica.net>
2014-03-07 14:57:04 +0100

    JS creating blocks when editing pages
    Right now it only creates blocks with the default template. Next step would be to be able to change a block layout and reload the fields

---
+++ app/assets/javascripts/no_cms/admin/pages/content_block_handler.js
@@ -0,0 +1,48 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+
+NoCMS = {};
+
+NoCMS.ContentBlockHandler = function() {
+
+
+  var default_layout_block = $('#new_content_block_default'),
+    block_placeholder = $('#content_blocks_placeholder'),
+    new_content_link = $('#new_content_block'),
+    block_templates = $('.new.block'),
+    that = this;
+
+  new_content_link.on('click', function(e){
+    e.preventDefault();
+    that.createBlock();
+  });
+
+
+  this.createBlock = function(){
+    new_block = default_layout_block.clone();
+    new_block.removeClass('new');
+    new_block.removeAttr('id');
+    this.modifyInputNames(new_block, $('.block').not('.new').length);
+    block_placeholder.append(new_block);
+  }
+
+  this.modifyInputNames = function(block, position){
+
+    block.find('[for]').each(function(){
+      $(this).attr('for', $(this).attr('for').replace(/_[0-9]*_/, '_'+position+'_'))
+    });
+    block.find('[id]').each(function(){
+      $(this).attr('id', $(this).attr('id').replace(/_[0-9]*_/, '_'+position+'_'))
+    });
+    block.find('[name]').each(function(){
+      $(this).attr('name', $(this).attr('name').replace(/\[[0-9]*\]/, '['+position+']'))
+    });
+
+  }
+
+
+  block_templates.each(function() {
+    $(this).detach();
+  });
+
+}
