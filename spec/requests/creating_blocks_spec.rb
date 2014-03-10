b1662a08bcf92ba1d730a307eb28d23984c60511
David J. Brenes <david.brenes@simplelogica.net>
2014-03-10 10:45:42 +0100

    Spec for creating a default layout block

---
+++ spec/requests/creating_blocks_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+
+describe NoCms::Admin::Pages do
+
+  context "when creating new blocks", js: true do
+
+    let(:nocms_page) { create :nocms_page }
+    let(:page_title) { Faker::Lorem.sentence }
+    let(:block_title) { Faker::Lorem.sentence }
+    let(:block_body) { Faker::Lorem.sentence }
+
+    subject { page }
+
+    before do
+
+      visit no_cms_admin_pages.edit_page_path(nocms_page)
+
+
+    end
+
+    it "generates new blocks" do
+
+      expect(page).to_not have_selector '.block'
+
+      click_link I18n.t('no_cms.admin.pages.blocks.index.new_block')
+
+      expect(page).to have_selector '.block'
+
+    end
+
+    it "creates new blocks when submitted" do
+
+      click_link I18n.t('no_cms.admin.pages.blocks.index.new_block')
+
+      within('.block') do
+        fill_in I18n.t('activerecord.attributes.no_cms/pages/block.title'), with: block_title
+        fill_in I18n.t('activerecord.attributes.no_cms/pages/block.body'), with: block_body
+      end
+
+      click_button(I18n.t('no_cms.admin.pages.pages.new.submit'))
+
+      visit nocms_page.path
+      expect(page).to have_selector('.title', text: block_title)
+      expect(page).to have_selector('.body', text: block_body)
+
+    end
+
+  end
+end
