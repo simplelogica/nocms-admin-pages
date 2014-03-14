9bd5055b3de8506ee25bf551c42a0d6fe4d51bd8
David J. Brenes <david.brenes@simplelogica.net>
2014-03-14 14:10:43 +0100

    Using draft attribute for classes in the secondary menu

--- app/helpers/no_cms/admin/pages/pages_helper.rb
+++ app/helpers/no_cms/admin/pages/pages_helper.rb
@@ -1,4 +1,10 @@
 module NoCms::Admin::Pages
   module PagesHelper
+    def page_listing_item_classes page, current_page
+      classes = []
+      classes << 'current' if current_page == page
+      classes << 'oculto' if page.draft
+      classes
+    end
   end
 end
