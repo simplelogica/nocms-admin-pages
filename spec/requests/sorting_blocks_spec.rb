1c997a2986e1dbf278c5ad84ff57f04189947db4
David J. Brenes <david.brenes@simplelogica.net>
2014-04-05 13:03:10 +0200

    Tested sorting nested blocks

--- spec/requests/sorting_blocks_spec.rb
+++ spec/requests/sorting_blocks_spec.rb
@@ -54,8 +54,60 @@ describe NoCms::Admin::Pages do
 
       end
     end
+  end
 
+  context "when sorting nested blocks", js: true do
 
+    let(:nocms_page) { create :nocms_page }
+    let(:block_container) { create :nocms_block, layout: 'container_with_background', page: nocms_page }
+    let(:first_block_nested) { create :nocms_block, layout: 'logo-caption', page: nocms_page, caption: Faker::Lorem.sentence, parent: block_container }
+    let(:second_block_nested) { create :nocms_block, layout: 'logo-caption', page: nocms_page, caption: Faker::Lorem.sentence, parent: block_container }
+
+    before do
+
+      first_block_nested
+      second_block_nested
+
+      visit no_cms_admin_pages.edit_page_path(nocms_page)
+
+    end
+    context "when moving down nested blocks", js: true do
+      before do
+        within("#content_block_#{first_block_nested.id}") do
+          click_link I18n.t('no_cms.admin.pages.blocks.form.move_down')
+        end
+
+        click_button(I18n.t('no_cms.admin.pages.pages.toolbar_right.submit'))
+      end
+
+      it "should sort the blocks" do
+
+        visit nocms_page.path
+
+        expect(page).to have_selector('.logo_block:first-of-type .caption', text: second_block_nested.caption)
+        expect(page).to have_selector('.logo_block:last-of-type .caption', text: first_block_nested.caption)
+
+      end
+    end
+
+    context "when moving up blocks", js: true do
+      before do
+        within("#content_block_#{second_block_nested.id}") do
+          click_link I18n.t('no_cms.admin.pages.blocks.form.move_up')
+        end
+
+        click_button(I18n.t('no_cms.admin.pages.pages.toolbar_right.submit'))
+      end
+
+      it "should sort the blocks" do
+
+        visit nocms_page.path
+
+        expect(page).to have_selector('.logo_block:first-of-type .caption', text: second_block_nested.caption)
+        expect(page).to have_selector('.logo_block:last-of-type .caption', text: first_block_nested.caption)
+
+      end
+    end
   end
 
 end
