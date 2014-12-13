f6f5ad71375eb6d72902e2d77f516cad4384aa82
David J. Brenes <davidjbrenes@gmail.com>
2014-12-13 17:18:02 +0100

    Moved blocks configuration to the NoCms::Blocks initializer

--- spec/dummy/config/initializers/nocms/pages.rb
+++ spec/dummy/config/initializers/nocms/pages.rb
@@ -22,44 +22,6 @@ NoCms::Pages.configure do |config|
   # }
   # config.block_layouts = {}
 
-  config.block_layouts = {
-    'default' => {
-      template: 'default',
-      fields: {
-        title: :string,
-        body: :text
-      },
-      nest_levels: [0]
-    },
-    'title-3_columns' => {
-      template: 'title_3_columns',
-      fields: {
-        title: :string,
-        column_1: :text,
-        column_2: :text,
-        column_3: :text
-      },
-      nest_levels: [0]
-    },
-    'logo-caption' => {
-      template: 'logo_caption',
-      fields: {
-        caption: :string,
-        logo: TestImage
-      },
-      nest_levels: [0,1]
-    },
-    'container_with_background' => {
-      template: 'container_with_background',
-      fields: {
-        corners_css_class: :string,
-        background_color: :string
-      },
-      allow_nested_blocks: true,
-      nest_levels: [0, 1, 2]
-    }
-
-  }
 
   # By default we use blocks to create the content of the page. If we just want a big textarea to insert the content we must set use_body to true
   # config.use_body = false
